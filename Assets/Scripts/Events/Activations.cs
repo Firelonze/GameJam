37c4714c61c935103d416cffe1c33aa451a8f8c6
Robin van den Bor <Firelonze2002@outlook.com>
2020-12-18 12:59:06 +0100

    Followers Added, Events Created
    Fix collision

---
+++ Assets/Scripts/Events/Activations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activations : MonoBehaviour
+{
+    [SerializeField] private GameObject[] followers;
+    private int AmountOfFollowers;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.tag == "Player" || transform.position.x < 142)
+        { 
+            Instantiate(followers[AmountOfFollowers]);
+            AmountOfFollowers++;
+        }
+        else
+        {
+            for (int i = 0; i < followers.Length; i++)
+            {
+                Destroy(followers[i]);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Events/Follower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField] private float speed = 15;
+    private Transform target;
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (transform.position.x + 20 > target.position.x || transform.position.x + 10 > target.position.x)
+        {
+            speed -= 3;
+        }
+    }
+
+}
